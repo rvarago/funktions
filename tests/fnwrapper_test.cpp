1296efbca4140bdfa25eaa02e96ea8cc0837d5b8
Rafael Varago <varago.rafael@gmail.com>
2020-07-08 22:29:33 +0200

    refactor: tests: fn_wrapper: Extract number generators into a function

--- tests/fnwrapper_test.cpp
+++ tests/fnwrapper_test.cpp
@@ -4,13 +4,19 @@
 
 using namespace rvarago::funktions;
 
+namespace {
+auto generate_random_ints() {
+    return GENERATE(take(100, random(-100, 100)));
+}
+}
+
 TEST_CASE("fn_wrapper wraps a generic callable that can be later called", "[fn_wrapper]") {
     auto const model = [](auto const x, auto const y) { return std::to_string(2 * x + y); };
 
     auto const model_wrapped = fn(model);
 
-    auto const x = GENERATE(take(100, random(-100, 100)));
-    auto const y = GENERATE(take(100, random(-100, 100)));
+    auto const x = generate_random_ints();
+    auto const y = generate_random_ints();
 
     CHECK(model_wrapped(x, y) == model(x, y));
 }
