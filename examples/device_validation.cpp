95d1ee0f97938eff2168ebe4b84a0fdf78290fd7
Rafael Varago <varago.rafael@gmail.com>
2020-10-13 11:55:01 +0200

    refactor: examples: Extract device registry creation into its own function

--- examples/device_validation.cpp
+++ examples/device_validation.cpp
@@ -23,19 +23,24 @@ struct device {
 
 }
 
-int main(int, char *[]) {
-    auto const device_registry = std::vector<device>{
+auto fetch_all_devices() -> std::vector<device> {
+    return std::vector<device>{
         {identifier{1}, vendor_identifier{1}, status::Idle},    {identifier{2}, vendor_identifier{1}, status::Busy},
         {identifier{3}, vendor_identifier{2}, status::Idle},    {identifier{4}, vendor_identifier{2}, status::Busy},
         {identifier{5}, vendor_identifier{2}, status::Waiting},
     };
+}
 
-    // Given a device d: d.vendor_id == 2 and d.current_status == status::Idle
+int main(int, char *[]) {
+    auto const device_registry = fetch_all_devices();
+
+    // Given a device d: d.vendor_id == 2 and d.current_status != status::Busy
     auto const search_query = fn(&device::vendor_id) >> eq(2) & fn(&device::current_status) >> ne(status::Busy);
 
-    auto const device_found = std::find_if(device_registry.cbegin(), device_registry.cend(), search_query);
+    auto const device = std::find_if(device_registry.cbegin(), device_registry.cend(), search_query);
 
-    assert(device_found->id == 3);
+    assert(device != std::cend(device_registry));
+    assert(device->id == 3);
 
-    std::cout << "Device ID: " << device_found->id << '\n';
+    std::cout << "Device ID: " << device->id << '\n';
 }
